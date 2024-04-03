df0d0274263eb430f35b22052370be3836f268c1
tiagoforward <devtiagoforward@gmail.com>
2024-04-03 12:37:58 -0300

    updating

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
+interface MeuBotaoProps {
+  texto: string
+}
+
+function MeuBotao(props: MeuBotaoProps){
+  return <button className="button bg-orange-400 h-10 px-3 rounded font-medium">{props.texto}</button>
+}
+
 export function App() {
 
   return (
-    <>
-      <h1>Hello Word</h1>
-    </>
+    <div className="flex gap-2">
+      <MeuBotao texto="clique aqui" />
+      <MeuBotao texto="clique aqui 2" />
+      <MeuBotao texto="2" />
+    </div>
   )
 }
