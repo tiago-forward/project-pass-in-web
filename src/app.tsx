d318ef3491c09e6104e3e2bba842eb09799af2af
tiagoforward <devtiagoforward@gmail.com>
2024-04-03 14:14:45 -0300

    creating the component header

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,12 @@
-interface MeuBotaoProps {
-  texto: string
-}
-
-function MeuBotao(props: MeuBotaoProps){
-  return <button className="button bg-orange-400 h-10 px-3 rounded font-medium">{props.texto}</button>
-}
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
 
 export function App() {
 
   return (
-    <div className="flex gap-2">
-      <MeuBotao texto="clique aqui" />
-      <MeuBotao texto="clique aqui 2" />
-      <MeuBotao texto="2" />
+    <div className="max-w-[1216px] mx-auto py-5">
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
